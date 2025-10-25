4703da8127765de4543144131e2c3d30cb065e95
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2025-10-25 22:09:11 +0200

    remove led-strip, switch back to LED

--- main.ts
+++ main.ts
@@ -3,25 +3,32 @@ namespace modules {
      * Calliope mini Neopixelstreifen an Servoboard C8
      */
     //% fixedInstance whenUsed block="Calliope Neopixelstrip C8"
-    //% block.loc.de="Calliope Neopixelstreifen"
-    export const CallipeNeopixelStrip1 = new LedStripClient("Calliope Neopixelstrip C8?dev=self&num_pixels=50&variant=Strip")
-
-
+    //% block.loc.de="Calliope Neopixelstreifen C8"
+    export const CallipeNeopixelStrip1 = new LedClient("Calliope Neopixelstrip C8?dev=self&num_pixels=50&variant=Strip")
+ 
+    //% fixedInstance whenUsed block="Calliope Neopixelstrip C9"
+    //% block.loc.de="Calliope Neopixelstreifen C9"
+    export const CallipeNeopixelStrip2 = new LedClient("Calliope Neopixelstrip C9?dev=self&num_pixels=50&variant=Strip")
 }
 namespace servers {
     function start() {
         jacdac.productIdentifier = 0x32690c10
-        jacdac.deviceDescription = "Calliope Neopixelstrip C8"
+        jacdac.deviceDescription = "Calliope Servoboard Neopixels"
         jacdac.startSelfServers(() => {
-            const pin = 108 //DigitalPin.C8
-            const sendPixels = (pixels: Buffer, brightness: number) => light.sendWS2812BufferWithBrightness(pixels, pin, brightness)
-            const servers = [
-                new jacdac.LedServer(
-                    50,
+       const servers = [
+                new jacdac.LedServer(50,
                     jacdac.LedPixelLayout.RgbGrb,
-                    sendPixels, {
-                    variant: jacdac.LedVariant.Strip
-                })
+                    (pixels, brightness) => light.sendWS2812BufferWithBrightness(pixels, DigitalPin.C8, brightness), {
+                    variant: jacdac.LedVariant.Strip,
+                    instanceName: "C8"
+                }),
+                new jacdac.LedServer(50,
+                    jacdac.LedPixelLayout.RgbGrb,
+                    (pixels, brightness) => light.sendWS2812BufferWithBrightness(pixels, DigitalPin.C9, brightness), {
+                    variant: jacdac.LedVariant.Strip,
+                    instanceName: "C9"
+                }
+                )
             ]
             return servers
         })
